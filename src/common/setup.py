f716bc90a6f65aad223ff3f5980370f1c9ab2362
l1sa3 <lisaengel2502@gmail.com>
2026-03-02 10:47:27 +0100

    Update submodules dragandbot_common and pylon-ros-camera to ROS2 branches

--- src/common/setup.py
+++ src/common/setup.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
+package_name = 'common'
+
+setup(
+    name=package_name,
+    version='0.0.0',
     packages=['face_detector', 'pulse_publisher', 'bdf_processor'],
     package_dir={'': 'src'},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Brian Gerkey',
+    maintainer_email='gerkey@example.com',
+    description='The talker_py package',
+    license='BSD',
 )
-setup(**d)
